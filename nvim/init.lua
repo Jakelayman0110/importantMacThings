8b7e592d02f3e974ede2944f09fad59f8252f102
Jakelayman0110 <99349809+Jakelayman0110@users.noreply.github.com>
2023-12-06 04:29:17 -0600

    Important Coding Things

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("jacobcs.core.options")
+require("jacobcs.core.keymaps")
+require("jacobcs.core.colorscheme")
+require("jacobcs.lazy")
---
+++ nvim/lua/jacobcs/plugins/init.lua
@@ -0,0 +1,6 @@
+return {
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigator
+	"vim-scripts/ReplaceWithRegister",
+	"nvim-lua/plenary.nvim",
+	"github/copilot.vim",
+}
